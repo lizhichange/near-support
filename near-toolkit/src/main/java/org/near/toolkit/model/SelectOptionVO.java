9353a6538b43a6fa3feb861bfd526a0d4dad235c
Your Name <lizhichange@163.com>
2020-06-07 12:21:21 +0800

    fix

---
+++ near-toolkit/src/main/java/org/near/toolkit/model/SelectOptionVO.java
@@ -0,0 +1,29 @@
+package org.near.toolkit.model;
+
+
+/**
+ * @author sunflower
+ */
+ public class SelectOptionVO extends ToString {
+    private String code;
+    private String desc;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+}
+
+
+
