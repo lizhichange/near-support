ce9aa470f1986f963de351a8a69f89ec254e8b3b
Your Name <lizhichange@163.com>
2021-02-13 20:17:11 +0800

    xx

--- near-toolkit/src/main/java/org/near/toolkit/common/DoMainUtil.java
+++ near-toolkit/src/main/java/org/near/toolkit/common/DoMainUtil.java
@@ -1,5 +1,6 @@
 package org.near.toolkit.common;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -33,5 +34,31 @@ public class DoMainUtil {
             return matcher.group();
         }
         return null;
+    }
+
+    /**
+     * 获取登录用户的IP地址
+     *
+     * @param request
+     * @return
+     */
+    public static String getIpAddr(HttpServletRequest request) {
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        if ("0:0:0:0:0:0:0:1".equals(ip)) {
+            ip = "127.0.0.1";
+        }
+        if (ip.split(",").length > 1) {
+            ip = ip.split(",")[0];
+        }
+        return ip;
     }
 }
