8249caa0a12b3c4a140aef65dfd6a7707b849fd6
Your Name <lizhichange@163.com>
2021-02-13 20:11:33 +0800

    xx

--- near-toolkit/src/main/java/org/near/toolkit/context/SessionContext.java
+++ near-toolkit/src/main/java/org/near/toolkit/context/SessionContext.java
@@ -19,6 +19,8 @@ public class SessionContext {
 
     public static final String OPEN_ID_KEY = "openId";
 
+    public static final String HOST_ADD = "hostAddress";
+
     /**
      * 将Session中存储的业务数据放到 SessionContext 工具类中直接访问，
      * 原理是使用线程变量ThreadLocal将参数存在来，同一个线程访问就可以获取到数据了。
@@ -36,15 +38,20 @@ public class SessionContext {
         if (StringUtil.isNotBlank(sessionData.getOpenId())) {
             ThreadCache.setAttribute(OPEN_ID_KEY, sessionData.getOpenId());
         }
+        if (StringUtil.isNotBlank(sessionData.getHostAdd())) {
+            ThreadCache.setAttribute(HOST_ADD, sessionData.getHostAdd());
+        }
 
     }
 
-    public static void set(HttpSession session, String userId, String openId) {
+    public static void set(HttpSession session, String userId, String openId,
+                           String hostAddress) {
         SessionData sessionData = new SessionData();
         sessionData.setUserId(userId);
         sessionData.setOpenId(openId);
         session.setAttribute(USER_ID_KEY, userId);
         session.setAttribute(OPEN_ID_KEY, openId);
+        session.setAttribute(HOST_ADD, hostAddress);
         writeSessionContext(sessionData);
     }
 
@@ -59,6 +66,14 @@ public class SessionContext {
         SessionData sessionData = new SessionData();
         sessionData.setOpenId(openId);
         session.setAttribute(OPEN_ID_KEY, openId);
+        writeSessionContext(sessionData);
+    }
+
+
+    public static void setHostAdd(HttpSession session, String hostAdd) {
+        SessionData sessionData = new SessionData();
+        sessionData.setHostAdd(hostAdd);
+        session.setAttribute(HOST_ADD, hostAdd);
         writeSessionContext(sessionData);
     }
 
@@ -82,7 +97,7 @@ public class SessionContext {
 
 
     /**
-     * 获取 userId，会在拦截器 AuthInterceptor 中注入到线程变量
+     * 获取 userId，会在拦截器  中注入到线程变量
      */
     public static String getUserId() {
         Object userId = ThreadCache.getAttribute(USER_ID_KEY);
@@ -90,6 +105,15 @@ public class SessionContext {
             return null;
         }
         return (String) userId;
+    }
+
+
+    public static String getHostAdd() {
+        Object hostAdd = ThreadCache.getAttribute(HOST_ADD);
+        if (hostAdd == null) {
+            return null;
+        }
+        return (String) hostAdd;
     }
 
     public static String getOpenId() {
@@ -107,6 +131,14 @@ public class SessionContext {
         }
 
         return (String) openId;
+    }
+
+    public static String getHostAdd(HttpServletRequest request) {
+        Object hostAdd = request.getSession().getAttribute(HOST_ADD);
+        if (hostAdd == null) {
+            return null;
+        }
+        return (String) hostAdd;
     }
 
     public static String getUserId(HttpServletRequest request) {
